9068f9e233e93448473da01a861c2a8ad6509eb0
SVVAAP <workwithsvvaao@gmail.com>
2024-06-25 21:47:00 +0530

    hii

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,55 +1,31 @@
 import React, { useState } from "react";
 import { FiMapPin, FiSearch } from "react-icons/fi";
 
-const Banner = ({handleInputChange,query}) => {
-    
+const Banner = ({ handleInputChange, query }) => {
+
   return (
+    <>
     <div className="max-w-screen-2xl container mx-auto xl:px-24 md:py-20 py-14 px-4">
       <h1 className="text-5xl font-bold text-primary mb-3">
-        Find your <span className="text-blue">new job</span> today
+        Find your <span className="text-blue">Dream job</span> today
       </h1>
       <p className="text-lg text-black/70 mb-8">
-        Thousands of jobs in the computer, engineering and technology sectors
-        are waiting for you.
+
       </p>
 
-      <form className="">
-        <div className="flex justify-start md:flex-row flex-col md:gap-0 gap-4">
-          <div className="flex md:rounded-s-md rounded shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 md:w-1/2 w-full">
-            <input
-              type="text"
-              name="username"
-              id="username"
-              className="block flex-1 border-0 bg-transparent py-1.5 pl-8 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-              placeholder="What position are you looking for ?"
-              onChange={handleInputChange}
-              value={query}
-            />
-            <FiSearch className="absolute mt-2.5 ml-2 text-gray-400" />
-          </div>
 
-          <div className="flex md:rounded-none rounded ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 md:w-1/3">
-            <input
-              type="text"
-              name="username"
-              id="username"
-              className="block flex-1 border-0 bg-transparent py-1.5 pl-8 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-              placeholder="Location"
-              onChange={handleInputChange}
-              value={""}
-            />
-            <FiMapPin className="absolute mt-2.5 ml-2 text-gray-400" />
-          </div>
-
-          <button
-            type="submit"
-            className="bg-blue py-2 px-8 text-white md:rounded-e-md md:rounded-s-none rounded"
-          >
-            Search
-          </button>
-        </div>
-      </form>
+      <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">Get back to growth with <span className=" text-blue dark:text-blue-500">the world's #1</span> JobHunt</h1>
+      <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Jobhunt4U Agency in Karnataka: Empowering careers, transforming lives, and shaping futures with dedication and excellence.</p>
+      <div class="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+        <a href="#" class="rounded-md bg-blue px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Get started</a>
+        <a href="#" class="text-sm font-semibold leading-6 text-black">Learn more <span aria-hidden="true">→</span></a>
+      </div>
+      
     </div>
+    <div className="flex flex-wrap justify-center">
+    <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-black md:text-5xl lg:text-6xl "><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Featured</span> Jobs.</h1>
+  </div>
+  </>
   );
 };
 
