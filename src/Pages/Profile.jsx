36a23c0e33585eadeaaf379be52dd404c91fd0bf
mrnayak25 <mrnayak27@gmail.com>
2024-07-31 14:10:40 +0530

    done fixing

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,60 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import { useJobs } from '../context/jobsContext';
-import Cookies from 'js-cookie'; // Import the js-cookie library
-import { signOut } from 'firebase/auth';
-import { auth, database, storage, ref, set, onValue, storageRef, uploadBytes, getDownloadURL } from '../firebase';
-import { useNavigate } from 'react-router-dom';
-import Card from '../components/Card';
-import { HiPencil } from 'react-icons/hi';
-import { useRef } from 'react';
+import React, { useEffect, useState } from "react";
+import { useJobs } from "../context/jobsContext";
+import Cookies from "js-cookie";
+import { signOut } from "firebase/auth";
+import { auth, database, storage, ref, set, storageRef, uploadBytes, getDownloadURL } from "../firebase";
+import { useNavigate } from "react-router-dom";
+import Card from "../components/Card";
+import { HiPencil } from "react-icons/hi";
 
 function Profile() {
-  const { user, jobs ,isLoading } = useJobs();
+  const { user, jobs, isLoading } = useJobs();
   const navigate = useNavigate();
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [isEditing, setIsEditing] = useState(false);
   const [filteredJobs, setFilteredJobs] = useState([]);
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    phone: '',
-    workStatus: '',
-    yearsOfExperience: '',
-    companyNames: '',
-    resume: '',
+    name: "",
+    email: "",
+    phone: "",
+    workStatus: "",
+    yearsOfExperience: "",
+    companyNames: "",
+    resume: "",
   });
 
   useEffect(() => {
     if (user) {
       setFormData({
-        name: user.name || '',
-        email: user.email || '',
-        phone: user.phone || '',
-        workStatus: user.workStatus || '',
-        yearsOfExperience: user.yearsOfExperience || '',
-        companyNames: user.companyNames || '',
-        resume: user.resumeUrl || '',
+        name: user.name || "",
+        email: user.email || "",
+        phone: user.phone || "",
+        workStatus: user.workStatus || "",
+        yearsOfExperience: user.yearsOfExperience || "",
+        companyNames: user.companyNames || "",
+        resume: user.resumeUrl || "",
       });
       const appliedJobs = user.appliedJobs || [];
       setFilteredJobs(jobs.filter((job) => appliedJobs.includes(job.id)));
 
-      const messageShown = Cookies.get('accountCreatedMessageShown');
+      const messageShown = Cookies.get("accountCreatedMessageShown");
       if (!messageShown) {
         setShowSuccessMessage(true);
-        // Set a cookie so the message is not shown again
-        Cookies.set('accountCreatedMessageShown', 'true', { expires: 365 }); // Cookie expires in 1 year
+        Cookies.set("accountCreatedMessageShown", "true", { expires: 365 });
       }
-      
     }
   }, [user, jobs]);
 
   const handleLogout = async () => {
     try {
+      if(confirm("Are you Sure You want to logout?")){
       await signOut(auth);
-      navigate('/'); // Redirect to the home page after logout
+      navigate("/");
+      }
     } catch (error) {
-      console.error('Error signing out: ', error);
+      console.error("Error signing out: ", error);
     }
   };
 
@@ -77,32 +76,30 @@ function Profile() {
     }));
   };
 
-  
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const user = auth.currentUser;
-      if (user) {
+      const currentUser = auth.currentUser;
+      if (currentUser) {
         let resumeUrl = formData.resume instanceof File ? await uploadResume(formData.resume) : formData.resume;
 
-        await set(ref(database, `users/${auth.currentUser.uid}`), {
+        await set(ref(database, `users/${currentUser.uid}`), {
           name: formData.name,
           email: formData.email,
           phone: formData.phone,
           workStatus: formData.workStatus,
           yearsOfExperience: formData.yearsOfExperience,
           companyNames: formData.companyNames,
-          resumeUrl: resumeUrl, // Update resume URL if file is uploaded
-          appliedJobs: user.appliedJobs,
-          userType: user.userType,
+          resumeUrl: resumeUrl,
+          appliedJobs: currentUser.appliedJobs ?? [],
+          userType: currentUser.userType?currentUser.userType:'candidate' ,
         });
 
         setIsEditing(false);
       }
     } catch (error) {
-      console.error('Error updating user details: ', error);
-      setError('Failed to update user details.');
+      console.error("Error updating user details: ", error);
+      setError("Failed to update user details.");
     }
   };
 
@@ -113,9 +110,9 @@ function Profile() {
       const downloadURL = await getDownloadURL(resumeRef);
       return downloadURL;
     } catch (error) {
-      console.error('Error uploading resume: ', error);
-      setError('Failed to upload resume.');
-      return '';
+      console.error("Error uploading resume: ", error);
+      setError("Failed to upload resume.");
+      return "";
     }
   };
 
@@ -125,20 +122,20 @@ function Profile() {
 
   return (
     <div className="container mx-auto p-4">
-     <button 
-      className="flex items-center px-4 py-2 text-black rounded-lg hover:font-extrabold focus:outline-none focus:ring-3 focus:ring-black"
-      onClick={() => URL("/")}
-    >
-      <span className="mr-2">{"<"}</span>
-      Back
-    </button>
-      {/* {error && <div className="text-red-500 mb-4">{error}</div>} */}
+      <div className="flex justify-between m-2">
+        <button
+          className="flex items-center px-4 py-2 text-black rounded-lg hover:font-extrabold focus:outline-none focus:ring-3 focus:ring-black"
+          onClick={() => navigate(-1)}>
+          <span className="mr-2">{"<"}</span>
+          Back
+        </button>
+        <button onClick={handleLogout} className="mt-4 bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600">
+          Logout
+        </button>
+      </div>
       <div className="relative bg-white shadow-md rounded-lg p-4">
         {!isEditing && (
-          <HiPencil
-            onClick={handleEditClick}
-            className="absolute top-4 right-4 text-blue-500 cursor-pointer text-xl"
-          />
+          <HiPencil onClick={handleEditClick} className="absolute top-4 right-4 text-blue-500 cursor-pointer text-xl" />
         )}
         <form onSubmit={handleSubmit} className="space-y-4">
           <h1 className="text-2xl font-bold mb-4">Welcome, {formData.name}!</h1>
@@ -149,15 +146,11 @@ function Profile() {
                 alt="Success"
                 className="w-8 h-8 mr-2"
               />
-              <span className="text-green-700 text-lg font-medium">
-                Your account is created successfully.
-              </span>
+              <span className="text-green-700 text-lg font-medium">Your account is created successfully.</span>
               <span className="text-gray-700 ml-2">Let’s get started!</span>
             </div>
           )}
-          <p className="text-lg mb-4">
-            Search & apply to jobs from India&apos;s No.1 Job Site
-          </p>
+          <p className="text-lg mb-4">Search & apply to jobs from India&apos;s No.1 Job Site</p>
           <div className="bg-gray-100 p-4 rounded-lg space-y-4">
             <div className="formField">
               <label className="block text-gray-700 mb-1">Full name</label>
@@ -166,7 +159,7 @@ function Profile() {
                 name="name"
                 value={formData.name}
                 onChange={handleChange}
-                className={`formInput p-2 border rounded w-full ${!isEditing ? 'bg-gray-200 cursor-not-allowed' : ''}`}
+                className={`formInput p-2 border rounded w-full ${!isEditing ? "bg-gray-200 cursor-not-allowed" : ""}`}
                 disabled={!isEditing}
               />
             </div>
@@ -178,7 +171,7 @@ function Profile() {
                 name="email"
                 value={formData.email}
                 onChange={handleChange}
-                className={`formInput p-2 border rounded w-full ${!isEditing ? 'bg-gray-200 cursor-not-allowed' : ''}`}
+                className={`formInput p-2 border rounded w-full ${!isEditing ? "bg-gray-200 cursor-not-allowed" : ""}`}
                 disabled={!isEditing}
               />
             </div>
@@ -190,7 +183,7 @@ function Profile() {
                 name="phone"
                 value={formData.phone}
                 onChange={handleChange}
-                className={`formInput p-2 border rounded w-full ${!isEditing ? 'bg-gray-200 cursor-not-allowed' : ''}`}
+                className={`formInput p-2 border rounded w-full ${!isEditing ? "bg-gray-200 cursor-not-allowed" : ""}`}
                 disabled={!isEditing}
               />
             </div>
@@ -201,15 +194,14 @@ function Profile() {
                 name="workStatus"
                 value={formData.workStatus}
                 onChange={handleChange}
-                className={`formInput p-2 border rounded w-full ${!isEditing ? 'bg-gray-200 cursor-not-allowed' : ''}`}
-                disabled={!isEditing}
-              >
+                className={`formInput p-2 border rounded w-full ${!isEditing ? "bg-gray-200 cursor-not-allowed" : ""}`}
+                disabled={!isEditing}>
                 <option value="fresher">I&apos;m a Fresher</option>
                 <option value="experienced">I&apos;m Experienced</option>
               </select>
             </div>
 
-            {formData.workStatus === 'experienced' && (
+            {formData.workStatus === "experienced" && (
               <>
                 <div className="formField">
                   <label className="block text-gray-700 mb-1">Years of Experience</label>
@@ -218,7 +210,9 @@ function Profile() {
                     name="yearsOfExperience"
                     value={formData.yearsOfExperience}
                     onChange={handleChange}
-                    className={`formInput p-2 border rounded w-full ${!isEditing ? 'bg-gray-200 cursor-not-allowed' : ''}`}
+                    className={`formInput p-2 border rounded w-full ${
+                      !isEditing ? "bg-gray-200 cursor-not-allowed" : ""
+                    }`}
                     disabled={!isEditing}
                   />
                 </div>
@@ -230,23 +224,24 @@ function Profile() {
                     name="companyNames"
                     value={formData.companyNames}
                     onChange={handleChange}
-                    className={`formInput p-2 border rounded w-full ${!isEditing ? 'bg-gray-200 cursor-not-allowed' : ''}`}
+                    className={`formInput p-2 border rounded w-full ${
+                      !isEditing ? "bg-gray-200 cursor-not-allowed" : ""
+                    }`}
                     disabled={!isEditing}
                   />
                 </div>
               </>
             )}
 
-<div className="formField">
+            <div className="formField">
               <label className="block text-gray-700 mb-1">Resume</label>
               {!isEditing && formData.resume && (
-                 <a
-                 href={formData.resume}
-                 target="_blank"
-                 rel="noopener noreferrer"
-                 className="text-blue-500 mt-2 block"
-               >
-                <b>Your Resume - click here to view</b>
+                <a
+                  href={formData.resume}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 mt-2 block">
+                  <b>Your Resume - click here to view</b>
                 </a>
               )}
               {isEditing && (
@@ -257,38 +252,19 @@ function Profile() {
                   className="formInput p-2 border rounded w-full"
                 />
               )}
-              {/* {formData.resume && (
-               
-                  <b>Your Resume - click here to view</b>
-            
-              )} */}
             </div>
 
             <div className="flex justify-between">
               {isEditing && (
-                <button
-                  type="submit"
-                  className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600"
-                >
+                <button type="submit" className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600">
                   Save Changes
                 </button>
               )}
             </div>
           </div>
         </form>
-
-        {/* Logout Button */}
-        {!isEditing && (
-          <button
-            onClick={handleLogout}
-            className="mt-4 bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600"
-          >
-            Logout
-          </button>
-        )}
       </div>
 
-      {/* Applied Jobs Section */}
       <div className="mt-8">
         <h2 className="text-xl font-semibold">Applied Jobs</h2>
         {filteredJobs.length > 0 ? (
